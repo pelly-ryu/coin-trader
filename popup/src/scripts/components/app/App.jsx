511e26e1fc8a0c2d9a6adad4eb4ed2a8601a4db5
Pelly Ryu <pelly.ryu@gmail.com>
2017-08-08 22:29:51 +0900

    feat: get Coinone balance with credentials

--- popup/src/scripts/components/app/App.jsx
+++ popup/src/scripts/components/app/App.jsx
@@ -1,70 +1,70 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import crypto from 'crypto-browserify'
 
 class CredentialForm extends Component {
   constructor (props) {
     super(props)
     this.state = {
       form: {
-        accessToken: 'default',
+        accessToken: '',
         secretKey: '',
-      }
+      },
+      result: '',
     }
   }
 
-  // getBalance () {
-  //   const crypto = require('crypto')
-  //   const request = require('request')
-  //   const ACCESS_TOKEN = ''
-  //   const SECRET_KEY = ''
-  //   const url = 'https://api.coinone.co.kr/v2/account/balance/'
-  //   let payload = {
-  //     'access_token': ACCESS_TOKEN,
-  //     'nonce': Date.now()
-  //   }
-  //
-  //   payload = new Buffer(JSON.stringify(payload)).toString('base64')
-  //
-  //   const signature = crypto
-  //     .createHmac('sha512', SECRET_KEY.toUpperCase())
-  //     .update(payload)
-  //     .digest('hex')
-  //
-  //   const headers = {
-  //     'content-type': 'application/json',
-  //     'X-COINONE-PAYLOAD': payload,
-  //     'X-COINONE-SIGNATURE': signature
-  //   }
-  //
-  //   const options = {
-  //     url: url,
-  //     headers: headers,
-  //     body: payload
-  //   }
-  //
-  //   request.post(options,
-  //     function (error, response, body) {
-  //       console.log(body)
-  //     })
-  // }
+  getBalance () {
+    const url = 'https://api.coinone.co.kr/v2/account/balance/'
+    let payload = {
+      'access_token': this.state.form.accessToken,
+      'nonce': Date.now()
+    }
+
+    payload = new Buffer(JSON.stringify(payload)).toString('base64')
+
+    const signature = crypto
+      .createHmac('sha512', this.state.form.secretKey.toUpperCase())
+      .update(payload)
+      .digest('hex')
+
+    const headers = {
+      'content-type': 'application/json',
+      'X-COINONE-PAYLOAD': payload,
+      'X-COINONE-SIGNATURE': signature
+    }
+
+    const axiosInstance = axios.create({
+      baseURL: url,
+      timeout: 10000,
+      headers: headers
+    })
+
+    axiosInstance.post('')
+      .then((response) => {
+        this.setState({result: JSON.stringify(response.data)})
+      })
+
+  }
 
   onChange (event) {
     this.state.form[event.target.name] = event.target.value
-    this.setState({form: this.state.form})
+    this.setState({form: this.state.form,})
   }
 
   onSubmit (event) {
     event.preventDefault()
 
-    //this.getBalance()
+    this.getBalance()
   }
 
   render () {
     return (
-      <form onSubmit={this.onSubmit}>
+      <form onSubmit={this.onSubmit.bind(this)}>
         Access Token <input type="text" name="accessToken" onChange={this.onChange.bind(this)}/><br/>
         Secret Key <input type="text" name="secretKey" onChange={this.onChange.bind(this)}/><br/>
         <input type="submit" value="load"/>
-        <br/><br/>your input : {this.state.form.accessToken}
+        <div>{this.state.result}</div>
       </form>
     )
   }
